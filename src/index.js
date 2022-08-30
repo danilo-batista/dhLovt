32f707c236edeeeda2a6003cfd0d3a7ece9be712
Danilo Batista <dbatistadsn@gmail.com>
2022-08-30 00:57:51 -0300

    feat: create the header with a responsive navigate menu.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const __dirname = path.resolve();
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", (request, response) => {
-    return response.json({ message: "Hello World!" });
+    return response.sendFile(path.join(__dirname, "src/views/index.html"));
 });
 
 app.listen(3333, () => {
